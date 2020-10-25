91189e1ecdbb5d77041e3a07439943ed23c0b901
Luiz Paulo <luiz.paulo.neto@hotmail.com>
2020-10-25 16:46:49 -0300

    Added worklist and changed color scheme

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 //Definindo variável para não causar erro de escopo
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = [];
+let workItems = [];
 
 //Iniciando Templates ejs
 app.set("view engine", "ejs");
@@ -16,7 +17,9 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
+
 //Resposta padrão root
 app.get("/", function(req, res) {
 
@@ -31,20 +34,33 @@ app.get("/", function(req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
   res.render("list", {
-    KindofDay: day,
+    listTitle: day,
     items: items
   });
 
 });
 
+
+//Lista de Trabalho
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work", items: workItems});
+});
+
+//Página de contato.
 app.get("/about", function(req, res){
   res.render("about")
 });
 
 app.post("/", function(req, res) {
   let item = req.body.item;
-  items.push(item);
-  res.redirect("/");
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 //Determina que o servidor rode na porta 3000 e loga o resultado.
